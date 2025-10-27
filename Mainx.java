3669ae5620237b512a7650b6ea128ad77614ef96
Prakash <pp683095@gmail.com>
2025-10-27 15:44:02 +0530

    Update Mainx.java

--- Mainx.java
+++ Mainx.java
@@ -1,24 +1,11 @@
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
 
-public class Main extends Application {
-    @Override
+public class Mainx {
+
     public void start(Stage stage) {
-        Button btn = new Button("Click Me!");
-        btn.setOnAction(e -> btn.setText("Hello from JavaFX!"));
-
-        StackPane root = new StackPane(btn);
-        Scene scene = new Scene(root, 400, 200);
-
-        stage.setTitle("Simple JavaFX App");
-        stage.setScene(scene);
-        stage.show();
+       System.out.println("Hello world");
     }
 
     public static void main(String[] args) {
-        launch(args);
+
     }
 }
