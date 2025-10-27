d73e40255a5880442ecb21cefe8d62a6d4801fef
Prakash <pp683095@gmail.com>
2025-10-27 15:44:24 +0530

    Update Mainx.java

--- Mainx.java
+++ Mainx.java
@@ -1,11 +1,9 @@
 
 public class Mainx {
 
-    public void start(Stage stage) {
-       System.out.println("Hello world");
-    }
-
+    
+    
     public static void main(String[] args) {
-
+   System.out.println("Hello world");
     }
 }
